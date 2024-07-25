9c395705d31b5c4122ad4784aebcc97263345762
Langa Hoohlo <langahoohlo@gmail.com>
2024-07-25 20:32:11 +0200

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export function getListStudents() {
+  const students1 = {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  };
+  const students2 = {
+    id: 2,
+    firstName: "James",
+    location: "San Columbia",
+  };
+  const students3 = {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  };
+
+  const classRoom = [];
+
+  return classRoom.push(students1, students2, students3);
+}

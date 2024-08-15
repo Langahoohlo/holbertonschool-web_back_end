8391597255425d2107c631e7175994c967186766
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 10:52:22 +0200

    string concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module has function that returns sum of two floats
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        returns the sum of a and b
+
+        Args:
+            str1 (str): string
+            str2 (str): string
+
+        Returns:
+            concat of str1 and str2
+    """
+    return str1 + str2

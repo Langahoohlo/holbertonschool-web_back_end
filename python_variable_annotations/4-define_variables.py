db00a0c7fae8cabbfe36bd57eb0bec747ccdf3a4
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 11:50:05 +0200

    string concat

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module to define integer, float, bool and string.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

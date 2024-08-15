10f7ea3aa333ba0c52aa47a9d93256cff9906d95
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 11:00:10 +0200

    string concat

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module has function that returns sum of two floats
+"""
+
+
+def floor(flr: float) -> float:
+    """
+        returns the sum of a and b
+
+        Args:
+            flr (float): float
+
+        Returns:
+            a flr
+    """
+    return flr

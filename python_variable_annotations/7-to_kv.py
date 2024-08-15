f4cb412d7e1b87250a210e8818f274f520f654d0
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 12:54:45 +0200

    string concat

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module has a function that returns the sum of a list of floats.
+"""
+from typing import List, Union
+
+
+def to_kv(k: str, v: [int | float]) -> tuple:
+    """Function takes to args and returns them in tuple
+
+    Args:
+        k (str): string arg
+        v (int): int OR float arg
+
+    Returns:
+        tuple[str, int]: _description_
+    """
+    return (k, v ** 2)

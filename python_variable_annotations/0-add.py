334a9ab5e4d9e818b56e0b9a9bdc0a248fa84f96
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 10:45:09 +0200

    type annotation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,8 +2,9 @@
 
 def add(a: float, b: float) -> float:
     """
-        :param a: parameter to add
-        :param b: parameter to add
-        :return: return both parameters added
+        :param a(float) parameter to add
+        :param b(float) parameter to add
+
+        :return return both parameters added
     """
     return a + b

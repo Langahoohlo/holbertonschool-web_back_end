ab5d3f61e8db507a2dbaf1cd61d6af051e13c176
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 11:15:07 +0200

    string concat

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module has function that returns sum of two floats
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        returns the sum of a and b
+
+        Args:
+            n (float): float
+
+        Returns:
+            a str
+    """
+    return str(n)

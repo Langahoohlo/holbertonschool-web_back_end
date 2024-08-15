0c13673d8d762c99a45e2fc0846acd0962853ef5
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-15 11:54:23 +0200

    string concat

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module has function that returns sum of two floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+        returns the sum of a and b
+
+        Args:
+            sum_list (float): float
+
+        Returns:
+            :param input_list:
+    """
+
+    return sum(input_list)

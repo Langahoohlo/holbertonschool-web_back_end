af2cc1fcdc9a05d2919fc2aca5aea7fe49c06064
langahoohlo <langahoohlo@gmail.com>
2024-08-18 16:14:07 +0200

    task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""_summary_
+
+Import: 
+    random: Random number between input
+    asyncio: To make the function asynchronous
+
+Returns:
+    _type_: _description_
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """_summary_
+
+    Args:
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        float: _description_
+    """
+    min_value: int = 0
+    max_value: int = max_delay
+    random_integer: int = random.uniform(min_value, max_value)
+    await asyncio.sleep(float(random_integer))
+
+    return random_integer

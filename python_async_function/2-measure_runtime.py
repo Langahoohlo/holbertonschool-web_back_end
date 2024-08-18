377f38150a19aa2256b8e511479f98cb4ea91da3
Langa Hoohlo <langahoohlo@gmail.com>
2024-08-18 20:27:55 +0200

    task 0

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
-"""Module contains function that checks async run time
 
-Imports:
-    1-concurrent_coroutines: async function to check
-    time: time module for getting time elapsed
-    asyncio: async module
 """
-import time
+    contains a function that measures the runtime when using wait_n
+    Imports:
+        asyncio
+        time
+        wait_n
+"""
+
 import asyncio
-wait_n = __import__('1-concurrent_coroutines').wait_n
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """Measure the total runtime of running the `wait_n` function `n` times with a maximum delay of `max_delay`.
-    
-    Args:
-        n (int): The number of times to run `wait_n`.
-        max_delay (int): The maximum delay for each call to `wait_n`.
-    
-    Returns:
-        float: The total runtime divided by the number of calls to `wait_n`.
+def measure_time(n: int, max_delay: int = 10) -> float:
     """
-        
+        measures the runtime of using wait_n
+        Args:
+            n (int): first arg for wait_n
+            max_delay (int): sec arg for wait_n
+        Returns:
+            a float number approximate to runtime
+    """
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     elapsed_time = time.perf_counter() - start_time
     return elapsed_time
-
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))

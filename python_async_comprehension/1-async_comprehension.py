7394e3982cc40b9b4567843423136b6e64691954
langahoohlo <langahoohlo@gmail.com>
2024-08-21 13:39:16 +0200

    async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
 from typing import List
-import asyncio
+
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
+    """
+    Async Comprehensions
+
+    Returns:
+        List[float]: _description_
+    """
     numbers = [num async for num in async_generator()]
     return numbers

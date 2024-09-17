e71d8148a89e93eabdcff6996ed3e608d28669c5
Langa Hoohlo <langahoohlo@gmail.com>
2024-09-17 04:20:22 +0200

    http express

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-export function countStudents(path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
